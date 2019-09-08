9325f59112eb810f4cc28a88b57b28ded6d5d393
Matthew Wall <mwall@lancet.mit.edu>
2019-09-08 10:17:04 -0400

    import from github

---
+++ install.py
@@ -0,0 +1,19 @@
+# installer for the weewx-sdr driver
+# Copyright 2016-2019 Matthew Wall
+# Distributed under the terms of the GNU Public License (GPLv3)
+
+from setup import ExtensionInstaller
+
+def loader():
+    return SDRInstaller()
+
+class SDRInstaller(ExtensionInstaller):
+    def __init__(self):
+        super(SDRInstaller, self).__init__(
+            version="0.63",
+            name='sdr',
+            description='Capture data from rtl_433',
+            author="Matthew Wall",
+            author_email="mwall@users.sourceforge.net",
+            files=[('bin/user', ['bin/user/sdr.py'])]
+            )
